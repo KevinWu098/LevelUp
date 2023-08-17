0699713aee13432d82b7e197829bb9e8a7d27c9a
Kevin Wu <kevinwu098@gmail.com>
2023-08-17 16:12:33 -0700

    Implemented basic gig functionality

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -1,3 +1,65 @@
-export const deleteUser = (req, res) => {
-    res.send("from controller");
+import Gig from "../models/gig.model.js";
+import createError from "../utils/createError.js";
+
+export const createGig = async (req, res, next) => {
+    if (!req.isSeller) {
+        return next(createError(403, "Only sellers can create a gig!"));
+    }
+
+    const newGig = new Gig({
+        userId: req.useId,
+        ...req.body,
+    });
+
+    try {
+        const savedGig = await newGig.save();
+
+        res.status(201).json(savedGig);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteGig = async (req, res, next) => {
+    try {
+        const gig = await Gig.findById(req.params.id);
+
+        if (gig.userId !== req.userId) {
+            return next(createError(403, "You can only delete your own gig!"));
+        }
+
+        await Gig.findByIdAndDelete(req.params.id);
+
+        res.status(200).send("Gig has been deleted!");
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getGig = async (req, res, next) => {
+    try {
+        const gig = await Gig.findById(req.params.id);
+
+        if (!gig) {
+            next(createError(404, "Gig not found!"));
+        }
+
+        res.status(200).send(gig);
+    } catch (error) {
+        next(err);
+    }
+};
+
+export const getGigs = async (req, res, next) => {
+    try {
+        const gigs = await Gig.find();
+
+        if (!gigs) {
+            return next(createError(403, "No gigs found!"));
+        }
+
+        res.status(200).send(gigs);
+    } catch (error) {
+        console.log(error);
+    }
 };
