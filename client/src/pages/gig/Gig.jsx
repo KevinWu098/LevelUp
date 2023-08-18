377d8e12e3346e96df1f5e8cc1f2cbef608a25cc
Kevin Wu <kevinwu098@gmail.com>
2023-08-17 20:52:03 -0700

    Implemented reviews system

--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -4,6 +4,7 @@ import { Slider } from "infinite-react-carousel/lib";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
+import Reviews from "../../components/reviews/Reviews";
 
 function Gig() {
     const { id } = useParams();
@@ -182,7 +183,7 @@ function Gig() {
                                 </div>
                             </div>
                         )}
-                        {/* <Reviews gigId={id} /> */}
+                        <Reviews gigId={id} />
                     </div>
                     <div className="right">
                         <div className="price">
