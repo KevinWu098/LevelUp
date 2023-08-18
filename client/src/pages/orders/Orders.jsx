29a1431d0b41c3dd2431f86eb1c5c11eb7b6c38a
Kevin Wu <kevinwu098@gmail.com>
2023-08-18 15:28:17 -0700

    Add msg and conversation functionality. Minor styling

--- client/src/pages/orders/Orders.jsx
+++ client/src/pages/orders/Orders.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Orders.scss";
 import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
 
 const Orders = () => {
     const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+    const navigate = useNavigate();
 
     const { isLoading, error, data } = useQuery({
         queryKey: ["orders"],
@@ -14,6 +16,27 @@ const Orders = () => {
                 return res.data;
             }),
     });
+
+    const handleContact = async (order) => {
+        const sellerId = order.sellerId;
+        const buyerId = order.buyerId;
+
+        const id = sellerId + buyerId;
+
+        try {
+            const res = await newRequest.get(`/conversations/single/${id}`);
+
+            navigate(`/message/${res.data.id}`);
+        } catch (error) {
+            if (error.response.status === 404) {
+                const res = await newRequest.post("/conversations/", {
+                    to: currentUser.seller ? buyerId : sellerId,
+                });
+
+                navigate(`/message/${res.data.id}`);
+            }
+        }
+    };
 
     return (
         <div className="orders">
@@ -55,7 +78,7 @@ const Orders = () => {
                                             className="message"
                                             src="./img/message.png"
                                             alt=""
-                                            // onClick={() => handleContact(order)}
+                                            onClick={() => handleContact(order)}
                                         />
                                     </td>
                                 </tr>
